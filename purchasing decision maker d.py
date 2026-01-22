3c9a12a785c024aa0bc7e85af84e1ffc995f1729
zeliexia1988 <zelie.xia@gmail.com>
2026-01-22 10:45:51 +0100

    Update purchasing decision maker d.py

--- purchasing decision maker d.py
+++ purchasing decision maker d.py
@@ -155,7 +155,7 @@ if contracts is not None:
             elif rule_contract_purchase(qty_input, package_choice, de_choice):
                 ref = get_contract_price_text(material_choice, de_choice, pn_choice, today)
                 if ref: result_text = "✅Decision: Application tarif contractuelle\n\n{ref}\n"
-                        result_text += "Elydan : Supposé en stock, Expédition sous 72H, faire valider le délai par fournisseur"
+                    result_text += "Elydan : Supposé en stock, Expédition sous 72H, faire valider le délai par fournisseur"
                 else:
                     result_text = "ℹ️ Decision: Contact Category Manager Achats (Zélie XIA) pour analyse spécifique."
             else:
@@ -193,6 +193,7 @@ if contracts is not None:
                     </button>
                 </a>
             ''', unsafe_allow_html=True)
+
 
 
 
